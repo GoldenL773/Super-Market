27eae0d453bf0ba04623922758ac36a21f0e4721
phankienzz <phankienk51@gmail.com>
2025-03-09 00:36:56 +0700

    1

--- SuperMarket/src/main/java/org/example/spring/supermarket/controller/LoginController.java
+++ SuperMarket/src/main/java/org/example/spring/supermarket/controller/LoginController.java
@@ -2,11 +2,14 @@ package org.example.spring.supermarket.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
-    @GetMapping
+    @GetMapping("/login")
     public  String login(){
-        return "/login.html";
+        return "login.html";
     }
+    @PostMapping("/login")
+
 }
