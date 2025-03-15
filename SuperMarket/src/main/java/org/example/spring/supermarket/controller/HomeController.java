65430553476dc898164bc2c9fb9af2442736b9f2
GoldenL773 <standtheword9@gmail.com>
2025-03-16 00:34:30 +0700

    fix css template

--- SuperMarket/src/main/java/org/example/spring/supermarket/controller/HomeController.java
+++ SuperMarket/src/main/java/org/example/spring/supermarket/controller/HomeController.java
@@ -2,8 +2,12 @@ package org.example.spring.supermarket.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+    @GetMapping("/home")
+    public String home() {
+        return "/shop";
+    }
 }
